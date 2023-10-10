1e3d1d3b2e2f46ca03ed37d4954bc83a7944dea4
Elisariane <elisarianeBS@gmail.com>
2023-10-10 10:57:00 -0300

    :sparkles: feat: Add lombok na entidade UserModel

---
+++ src/main/java/br/com/Elisariane/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package br.com.Elisariane.todolist.user;
+
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    public String username;
+    public String name;
+    public String password;
+
+}
